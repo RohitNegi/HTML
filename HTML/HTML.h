058eb8002ea0640e8bd22b492c41c9c840e45150
RohitNegi <rohitnegi017@gmail.com>
2019-12-23 12:23:44 +0530

    added

---
+++ HTML/HTML.h
@@ -0,0 +1,19 @@
+//
+//  HTML.h
+//  HTML
+//
+//  Created by Rohit Negi on 23/12/19.
+//  Copyright © 2019 Rohit Negi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTML.
+FOUNDATION_EXPORT double HTMLVersionNumber;
+
+//! Project version string for HTML.
+FOUNDATION_EXPORT const unsigned char HTMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTML/PublicHeader.h>
+
+
